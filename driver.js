0074e8469d5b5345a0a85a2f8e8e43e55574d902
Hiroaki Takisawa <takisawa@hit-start.jp>
2017-09-17 09:39:32 +0900

    Add packages: request, request-promise.

--- driver.js
+++ driver.js
@@ -3,6 +3,11 @@
 var lambda = require("./index");
 
 var event = {};
-var context = {};
+var context = {
+  done: function(error, message) {
+    console.log('[message] ' + message);
+    console.log('[error] ' + error);
+  }
+};
 
 lambda.handler(event, context);
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 'use strict';
 
+const request = require('request-promise');
+
 exports.handler = function(event, context) {
+  context.done(null, 'IP');
 }
