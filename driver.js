e4c74149950701e648d78cfac28085b78b41c7bd
Hiroaki Takisawa <takisawa@hit-start.jp>
2017-09-18 16:30:23 +0900

    Add prosess for getting my IP.

--- driver.js
+++ driver.js
@@ -5,7 +5,12 @@ var lambda = require("./index");
 var event = {};
 var context = {
   done: function(error, message) {
+    console.log('[context.done]');
     console.log('[message] ' + message);
+    console.log('[error] ' + error);
+  },
+  fail: function(error) {
+    console.log('[context.fail]');
     console.log('[error] ' + error);
   }
 };
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 'use strict';
 
+const env = process.env;
 const request = require('request-promise');
 
 exports.handler = function(event, context) {
-  context.done(null, 'IP');
+  sleep(parseInt(env.SLEEP_SEC))
+    .then(function() { return request(env.RETURN_CLIENT_IP_URL) })
+    .then(function(body) { context.done(null, body) })
+    .catch(function(err) { context.fail(err) })
+}
+
+/**
+ * 引数で指定された秒数 sleep する。
+ * @param sec {number}
+ * @return {Promise}
+ */
+function sleep(sec) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve('abc');
+    }, sec * 1000);
+  });
 }
